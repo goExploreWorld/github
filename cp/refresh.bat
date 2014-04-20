556ea0b9bd20238a213392a8155e6a8a37dd2c2d
goExploreWorld <1767704200@qq.com>
2014-04-20 17:40:46 +0800

    del cp
    del cp

--- cp/refresh.bat
+++
@@ -1,9 +0,0 @@
-f:
-cd F:\DATAMANAGE\cp\daletou
-awk -f 1dltDelNumber.awk 1dltNumberData.csv
-awk -f 2dltguanxi.awk 2dltData.csv
-pause
-cd F:\DATAMANAGE\cp\ssq
-awk -f 1ssqDelNumber.awk 1ssqNumberData.csv
-awk -f 2ssqguanxi.awk 2ssqData.csv
-pause
